fb54b5fab7dc5d83d7611522d636fdff6036c792
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-07 12:01:51 +0600

    Post mehtod Done

--- index.js
+++ index.js
@@ -24,12 +24,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("coffeeDB");
+        const coffee = database.collection("coffee");
+
+        app.post('/coffee', async (req, res) => {
+            const newCoffee = req.body;
+            console.log('coffee : ', newCoffee);
+            const result = await coffee.insertOne(newCoffee);
+            res.send(result);
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
