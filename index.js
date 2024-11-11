721d7990609f6ea07d1dd077064c7d4e2ad96dc6
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-11 09:37:48 +0600

    Add and Get coffee data

--- index.js
+++ index.js
@@ -24,15 +24,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        
 
+        await client.connect();
+        console.log("Connected to MongoDB");
+
+        
+        const coffeeCollection = client.db('coffeeDB').collection('coffees');
+
+        //save coffee in the DB
+        app.post('/coffees', async (req, res) => {
+            const coffee = req.body;
+            const result = await coffeeCollection.insertOne(coffee);
+            res.send(result)
+        })
+
+        //get all coffee data form BD
+        app.get('/coffees', async(req, res) => {
+            const coffees = await coffeeCollection.find().toArray();
+            res.send(coffees);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
